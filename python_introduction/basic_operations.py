0320ccfc6ee040b0ab134ba1f9aba6511cd8d62f
shoile azeez <shoabdulazeez@gmail.com>
2024-06-19 13:41:38 -0700

    python intoduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1=10
+print(number1)
+number2=5
+print(number2)
+Addition=number1+number2
+print(Addition)
+subtraction=number1-number2
+print(subtraction)
+multiplication=number1*number2
+print(multiplication)
+
+
