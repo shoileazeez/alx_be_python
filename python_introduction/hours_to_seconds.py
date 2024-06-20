5af1a3ab457a2c087f02d6ef317b63d7dd10d0ce
shoile azeez <shoabdulazeez@gmail.com>
2024-06-20 13:09:21 -0700

    hours_to_second

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 hours = 2
 print(hours)
-seconds = hours *3600
+seconds = hours * 3600
 print(seconds)
 print (f"(hours) hour(s) is (seconds) seconds")
 print (f"(2 hour(s) is 7200 seconds) : {hours}")
