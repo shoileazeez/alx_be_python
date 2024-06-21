3d7d8072b99dcbb6af295d5a741ccf8b3388baa7
shoile azeez <shoabdulazeez@gmail.com>
2024-06-20 23:10:50 -0700

    futiure_age_calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -2,8 +2,8 @@ name = input("What is your name? ")
 print("Hello,", name) 
 age1 = input("how old are you? :")
 print(age1 ,"years old")
-num1 = int(input("Enter your age: "))
-print(num1 ,"years old")
-num2 = int(input("Enter the second number: "))
-age = num1 + num2
+num2 = int(input("Enter your age: "))
+print(num2 ,"years old")
+num3 = int(input("Enter the second number: "))
+age = num2 + num3
 print(f" In 2050, you will be {age} years old")
