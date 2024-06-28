bb4d5cd71a83f3609a3dc6ae144d044cc03a28bc
shoileazeez <shoabdulazeez@gmail.com>
2024-06-28 04:23:38 -0700

    match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,4 @@
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+result = int(input("Choose the operation (+, -, *, /):. for num1 and num2))
+    print(The result is {result})
