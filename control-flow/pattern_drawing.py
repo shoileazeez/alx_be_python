3aa82719ef3a83e3e9b9e775a53c2f73cb947988
shoile azeez <shoabdulazeez@gmail.com>
2024-06-30 05:01:07 -0700

    pattern_drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,28 @@
+pattern_size =int(input ("Enter the size of the pattern: "))
+i = pattern_size
+while i == pattern_size:
+ for i in range(pattern_size):
+  for j in range(pattern_size):
+   print("*",end='')
+  print()
+
+
+
+#age = 0
+#while age < 18:
+  #age = int(input("Enter your age (must be 18 or older): "))
+
+#print("You are old enough to proceed.")
+
+
+
+
+
+        
+  # Outer loop controls the number of rows
+    #for j in range(i  +1):
+    # Inner loop prints asterisks for each row
+    #print("*", end="")
+  #print()  # Move to a new line after each row of asterisks
+  
+ 
