1a92bb27d23e51430514ca7152ff2fc29e77cd95
shoile azeez <shoabdulazeez@gmail.com>
2024-07-11 13:02:34 -0700

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,32 +1,26 @@
 # Python program to create Bankaccount class
 # with both a deposit() and a withdraw() function
 class BankAccount:
-	def __init__(self):
-		self.balance=0
-		print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
-
-	def deposit(self):
-		amount=float(input("Enter amount to be Deposited: "))
-		self.balance += amount
-		print("\n Amount Deposited:",amount)
-
-	def withdraw(self):
-		amount = float(input("Enter amount to be Withdrawn: "))
-		if self.balance>=amount:
-			self.balance-=amount
-			print("\n You Withdrew:", amount)
-		else:
-			print("\n Insufficient balance ")
-
-	def display(self):
-		print("\n Net Available Balance=",self.balance)
-
-# Driver code
-
-# creating an object of class
-# s = Bank_Account()
-
-# # Calling functions with that class object
-# s.deposit()
-# s.withdraw()
-# s.display()
+  def __init__(self,initial_balance=0.0):
+      self.account_balance = initial_balance
+      
+  def  deposit(self, amount):
+        # amount = input(float(f"enter the amount you will like to deposit: "))
+        self.account_balance += amount
+        return self.account_balance
+  def withdraw(self, amount): 
+        # amount = input(float(f"enter the amount you would like to withdraw: "))
+        if self.account_balance >= amount:
+             return False
+        else:
+            self.account_balance <= amount
+            return True
+  def display_balance(self):
+      print(f"current Balance: {self.account_balance} ")
+      return 
+  
+           
+            
+            
+          
+            
