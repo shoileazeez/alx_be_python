4288e334a921305e568ce9772b7657f7c7c31eac
shoile azeez <shoabdulazeez@gmail.com>
2024-07-16 14:05:53 -0700

    my first book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,6 +11,6 @@ class Book:
     def __str__(self):
         return  f"{self.title} by {self.author}, published in {self.year}"
     def __repr__(self):
-        return f"Book('{self.title}', go'{self.author}', {self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
         
        
