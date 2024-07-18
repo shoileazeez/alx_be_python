b1e87043eab6d075151d344907826bf14f504744
shoile azeez <shoabdulazeez@gmail.com>
2024-07-18 08:21:29 -0700

    nice code

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,30 +2,24 @@ class Book:
     def __init__(self, title, author ):
         self.title = title
         self.author = author
-  
-        
-    def __str__(self):
-        return f" {self.title,}  by  {self.author} "   
+        print(f"Book: ", self.title, " by " , self.author)  
+    # def __str__(self):
+    #     return f"Book: ", self.title, " by " , self.author 
+            
 
 
 class EBook(Book):
     def __init__(self, title, author, file_size ):
         super().__init__(title, author)
         self.file_size = file_size
-        
-    def __str__(self):
-        return f"{super().__str__()}  [EBook, : ] " + self.file_size
+        print(f"EBook: {title} by {author}, file size: {file_size}KB")
         
         
 class PrintBook(Book):
     def __init__(self, title, author, page_count ):
         super().__init__(title, author)
         self.page_count = page_count
-        
-        
-    def __str__(self):
-        return f"{super().__str__()}  [PrintBook, :] "  +self.page_count   
-        
+        print(f"PrintBook:" ,self.title , self.author, "Page Count: " ,page_count)
         
         
 class Library():
@@ -38,14 +32,30 @@ class Library():
     def add_book(self, book):
         if isinstance(book, Book):
             self.books.append(book) 
+           
                
+        # else:
+        #     raise TypeError ("Only Book objects can be added to the library") 
+        
+        if isinstance(book, EBook):
+            self.books.append(book) 
+            
+            
+        elif isinstance(book, PrintBook):
+            self.books.append(book)      
+            
+            
         else:
-            raise TypeError ("Only Book objects can be added to the library")     
+            pass
+             
        
     def list_books(self):
         for book in self.books:
             return book
         
+        
+        
+
         
         
 # library = Library()
@@ -68,5 +78,62 @@ class Library():
 
 # library.list_books()
         
+        
+        
+        
+# class Book:
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
+
+#     # def __str__(self):
+#     #     return f" {self.title,}  by  {self.author} "   
+
+
+# class EBook(Book):
+#     def __init__(self, title, author, file_size):
+#         super().__init__(title, author, file_size)
+#         self.file_size = file_size
+
+
+# class PrintBook(Book):
+#     def __init__(self, title, author, page_count ):
+#         super().__init__(title, author, page_count)
+#         self.page_count = page_count   
+        
+        
+        
+        
+        
+# class Library():
+#     # def __init_subclass__(cls) -> None:
+#     #     return super().__init_subclass__()
+#     def __init__(self):
+#          self.books = []
+        
+        
+#     def add_book(self, book):
+#         if isinstance(book, Book):
+#             self.books.append(book) 
+               
+#         # else:
+#         #     raise TypeError ("Only Book objects can be added to the library") 
+        
+#         if isinstance(book, EBook):
+#             self.books.append(book) 
+            
+            
+#         elif isinstance(book, PrintBook):
+#             self.books.append(book)      
+            
+            
+#         else:
+#             raise TypeError ("Only Book objects can be added to the library") 
+             
+       
+#     def list_books(self):
+#         for book in self.books:
+#             return book
+                     
         
             
