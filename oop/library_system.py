aafdb703c12858fd04e03d40e3a3229b113b1d4e
shoile azeez <shoabdulazeez@gmail.com>
2024-07-18 09:11:03 -0700

    nice code

--- oop/library_system.py
+++ oop/library_system.py
@@ -15,7 +15,7 @@ class EBook(Book):
     def __init__(self, title, author, file_size ):
         super().__init__(title, author)
         self.file_size = file_size
-        print(f"EBook: {title} by {author}, File size: {file_size}KB")
+        print(f"EBook: {title} by {author}, File Size: {file_size}KB")
         pass
         
         
