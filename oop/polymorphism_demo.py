b696c8059319b2d191d937cf347e6d985b4f8b98
shoile azeez <shoabdulazeez@gmail.com>
2024-07-18 09:52:57 -0700

    my first polymorphism code

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,30 +1,53 @@
-from math import pi
 class Shape:
-    def area(self, good):
-        self.good = good 
+    def area(self): 
         raise NotImplementedError
+    
+    def description(self):
+        return "This is a shape"
     
     
 class Rectangle(Shape):
-    def area(self, length, width):
-        super().area()
-        self.length = length
+    def __init__(self, width, height):
+        # super().__init__()
         self.width = width
-        self.area = length * width
-        return "The area of the Rectangle is: " +self.area
-    
+        self.height = height
+        
+        
+    def area(self):
+        return self.width * self.height
     
     
 class Circle(Shape):
-    def area (self, radius):
-        super().area()
+    def __init__(self, radius):
         self.radius = radius
-        self.area = pi * +self.radius **2 
-        return "The area of the Circle is: " + self.area
+        
+        
+    def area(self):
+        import math
+        return math.pi * self.radius ** 2    
+        
+          
+    
+    # def description(self):
+    #     return"This is a rectangle"
+    
+    
+# def print_shape_info(shape):
+#     print(shape.description())
+#     print(f"Area: {shape.area()}") 
+    
+    
+# rect = Rectangle(4, 5)
+
+# print_shape_info(rect)
+    
+
+    # def area (self, radius):
+    #     super().area()
+    #     self.radius = radius
+    #     self.area = pi * +self.radius **2 
+    #     return "The area of the Circle is: " + self.area
      
      
      
      
-Rectangle.area()
-Circle.area()
-Shape = Rectangle(Circle())   
